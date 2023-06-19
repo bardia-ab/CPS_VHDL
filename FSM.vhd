3fcfbf546146d89803db8ff53b51f34eea159c76
Bardia Babaei <bardia.babaei@postgrad.manchester.ac.uk>
2023-06-19 09:34:25 +0100

    add i_Reset and o_CE_Cntr

--- FSM.vhd
+++ FSM.vhd
@@ -15,6 +15,7 @@ entity FSM is
 		i_Psclk1		:	in		std_logic;
 		i_Psclk2		:	in		std_logic;
 		i_Start			:	in		std_logic;
+		i_Reset			:	in		std_logic;
 		i_Locked1		:	in		std_logic;
 		i_Locked2		:	in		std_logic;
 		i_Locked3		:	in		std_logic;
@@ -29,6 +30,7 @@ entity FSM is
 		o_Reset2		:	out		std_logic;
 		o_Reset3		:	out		std_logic;
 		o_CE_CUT		:	out		std_logic;
+		o_CE_Cntr		:	out		std_logic;
 		o_CLR_Cntr		:	out		std_logic;
 		o_Shift_Value	:	out		std_logic_vector(get_log2(56 * g_O2 * g_N_Sets) - 1 downto 0);
 		o_Slct_Mux		:	out		std_logic_vector(get_log2(g_N_Segments+1) - 1 downto 0);
@@ -44,19 +46,22 @@ architecture behavioral of FSM is
 			g_Counter_Width	:	integer
 		);
 		port(
-			i_Clk		:	in		std_logic;
-			i_Start		:	in		std_logic;
-			i_Locked	:	in		std_logic;
-			i_Enable	:	in		std_logic;
-			o_CE_CUT	:	out		std_logic;
-			o_CLR_Cntr	:	out		std_logic;
-			o_Done		:	out		std_logic
+			i_Clk		:	in		std_logic; 
+			i_Reset		:	in		std_logic; 
+			i_Start		:	in		std_logic; 
+			i_Locked	:	in		std_logic; 
+			i_Enable	:	in		std_logic; 
+			o_CE_CUT	:	out		std_logic; 
+			o_CE_Cntr	:	out		std_logic; 
+			o_CLR_Cntr	:	out		std_logic; 
+			o_Done		:	out		std_logic 
 		);
 	end component;
 		
 	component CM_FSM
 		port(
 			i_Clk		:	in		std_logic;
+			i_Reset		:	in		std_logic;
 			i_Enable	:	in		std_logic;
 			i_Psdone	:	in		std_logic;
 			o_Psen		:	out		std_logic;
@@ -91,11 +96,13 @@ begin
 	CUT_FSM_Inst	:	CUT_FSM
 		generic map(g_Counter_Width => g_Counter_Width)
 		port map(
-			i_Clk		=>	i_Clk_Launch,		
+			i_Clk		=>	i_Clk_Launch,
+			i_Reset		=>	i_Reset,
 		    i_Start		=>	i_Start,		
 		    i_Locked	=>	r_Locked,	
 		    i_Enable	=>	r_En_CUT,	
 		    o_CE_CUT	=>	o_CE_CUT,	
+			o_CE_Cntr	=>	o_CE_Cntr,
 		    o_CLR_Cntr	=>	o_CLR_Cntr,	
 		    o_Done		=>	r_Done_CUT		
 		);
@@ -103,6 +110,7 @@ begin
 	CM1_FSM_Inst	:	CM_FSM
 		port map(
 			i_Clk		=>	i_Psclk1,
+			i_Reset		=>	i_Reset,
 			i_Enable	=>	r_En_CM1,
 			i_Psdone	=>	i_Psdone1,
 			o_Psen		=>	o_Psen1,
@@ -112,14 +120,15 @@ begin
 	CM2_FSM_Inst	:	CM_FSM
 		port map(
 			i_Clk		=>	i_Psclk2,
+			i_Reset		=>	i_Reset,
 			i_Enable	=>	r_En_CM2,
 			i_Psdone	=>	i_Psdone2,
 			o_Psen		=>	o_Psen2,
 			o_Done		=>	r_Done_CM2
 		);
 	
-	FSM_Controller_Set_Inst	:	entity work.FSM_Controller_Set
---	FSM_Controller_Inc_Inst	:	entity work.FSM_Controller_Inc
+	-- FSM_Controller_Set_Inst	:	entity work.FSM_Controller_Set
+	FSM_Controller_Inc_Inst	:	entity work.FSM_Controller_Inc
 		generic map(
 		g_O2			=>	g_O2,
 		g_N_Sets		=>	g_N_Sets,
